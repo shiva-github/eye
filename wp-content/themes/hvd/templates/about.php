8298d2dc34122e7d1214f1aefde2a004143bc84c
Shiva Shirbhate <shiva-github@github.com>
2019-05-28 18:36:37 +0530

    db update and contact page update

--- wp-content/themes/hvd/templates/about.php
+++ wp-content/themes/hvd/templates/about.php
@@ -70,46 +70,6 @@ get_header();
 					</div>
 				</div>
 			</div>
-
-
-			<div class="common-wrapper clear-both">
-				
-				<div class="row">
-					<div class="col-md-4">
-						<div class="success-name">Nikita Santosh Jadhav </div>
-						<div class="success-desig color-dgrey ">Student- Satara, Maharashtra, India</div>
-						<div class="success-desc">
-
-						<p>Nikita Santosh Jadhav, 15-year-old girl from Bhima Koregaon, Satara, had a squint in both eyes since birth. It did not seem to be a major problem initially, however, as she grew older, it her squint continued to increase, and started aﬀecting her vision. It started aﬀecting her ability to study. Nikita’s mother wanted to treat her, but was unable to do so because of ﬁnancial issues as well as issues related to her own health. In addition, Nikita’s squint not only aﬀected her studies, but also her social life. Despite their ﬁnancial issues, Nikita’s old parents planned her surgery, however, it had to be postponed as she caught a cold.</p> 
-
-						<p>Shortly afterwards, H.V. Desai Eye Hospital organised a camp at Nikita’s school, following which she was referred to H.V. Desai Eye Hospital for detail screening. After the screening it was recommended for her to undergo Squint Vertical. Few days later, she underwent surgery free of cost with the help of SBI life insurance. She was very happy when she came for her follow-up. And her mother was very satisﬁed with the result. Nikita and her mother are extremely grateful towards SBI Life and H.V. Desai Eye Hospital.</p>  
-
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="success-name">Vasant Balid</div>
-						<div class="success-desig color-dgrey ">Farmer- Junnar, Maharashtra, India</div>
-						<div class="success-desc">
-							<p>
-						Vasant is a poor farmer with the responsibility of his old mother, wife and three daughters. His vision problem hampered his routine life as well as his ability to work on his farm. Vasant complained of low vision for over a year. A local surgeon had diagnosed him with cataract, and had advised Vasant to undergo surgery for the same.</p> 
-						<p>
-						Soon after that, Vasant underwent screening at a camp conducted by H.V. Desai Eye Hospital in his district, with the support of Wonderworks & Vision. At the camp, he was diagnosed to have cataract in both eyes. The camp referred him for surgery for the same. Support from Wonderworks & Vision enabled Vasant to undergo cataract surgery free of cost at the H.V. Desai Eye Hospital. This surgery enabled Vasant to resume his day-to-day acitivites and continue to earn a living to support his family. He showed immense gratitude towards the hospital as well as the hospital for their kind and supportive behaviour throughout the entire process!</p>
-
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="success-name">Nikita Santosh Jadhav </div>
-						<div class="success-desig color-dgrey ">Student- Satara, Maharashtra, India</div>
-						<div class="success-desc">
-							<p>
-						Nikita Santosh Jadhav, 15-year-old girl from Bhima Koregaon, Satara, had a squint in both eyes since birth. It did not seem to be a major problem initially, however, as she grew older, it her squint continued to increase, and started aﬀecting her vision. It started aﬀecting her ability to study. Nikita’s mother wanted to treat her, but was unable to do so because of ﬁnancial issues as well as issues related to her own health. In addition, Nikita’s squint not only aﬀected her studies, but also her social life. Despite their ﬁnancial issues, Nikita’s old parents planned her surgery, however, it had to be postponed as she caught a cold.</p>
-                        <p>
-						Shortly afterwards, H.V. Desai Eye Hospital organised a camp at Nikita’s school, following which she was referred to H.V. Desai Eye Hospital for detail screening. After the screening it was recommended for her to undergo Squint Vertical. Few days later, she underwent surgery free of cost with the help of SBI life insurance. She was very happy when she came for her follow-up. And her mother was very satisﬁed with the result. Nikita and her mother are extremely grateful towards SBI Life and H.V. Desai Eye Hospital.</p>  
-						</div>
-					</div>
-				</div>
-			</div>
-
 		</div>
 	</div>
 </div>
--- wp-content/themes/hvd/templates/contact.php
+++ wp-content/themes/hvd/templates/contact.php
@@ -27,30 +27,9 @@ get_header();
 						<div class="common-about-summary clear-both">
 							<?php  echo $about_summary; ?>
 						</div>
-						<div class="contact-form-box pl-5 pr-5 pt-5 pb-5">
-							<div class="contact-getintouch">Get in Touch</div>
-							<div class="form-row">
-								<div class="mt-3 form-group w-50 pr-3">
-									
-									<input type="text" id="materialContactFormName" placeholder="Name" class="form-control border-top-0 border-right-0 border-left-0">
-								</div>
-
-
-								<div class="mt-3 form-group w-50 pl-3">
-									
-									<input type="email" id="materialContactFormEmail" placeholder="E-mail" class="form-control border-top-0 border-right-0 border-left-0">
-								</div>
-
-								<div class="mt-3 form-group col-12">
-									<label for="materialContactFormMessage">Message</label>
-									<textarea id="materialContactFormMessage" class="form-control md-textarea" rows="4"></textarea>
-								</div>
-								<div class="col-12">   
-									<button class="btn btn-info learn-more" type="submit">Submit</button>
-								</div>
-							</div>
-
-						</div>
+						<?php 
+						echo do_shortcode( '[contact-form-7 id="352" title="Contact form 1"]');
+?>
 					</div>
 					<div class="col-md-6">
 						<h1 class="about-title"><?php echo the_title(); ?></h1>
